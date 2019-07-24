209d019e6d7ee84e18dfcedbe30138bd25bc160f
jinyoung <bmdj123@gmail.com>
2019-07-25 02:40:51 +0900

    27. Fetching cities and rendering them

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, ScrollView, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { Constants }  from 'expo';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      cities: [],
+    };
+  }
   componentDidMount(){
     fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
     .then(response=>response.json())
-    .then(console.log);
+    .then(cities => {
+      console.log('cities =',cities.length);
+      this.setState({
+        cities
+      });
+    });
   }
 
-  renderItem({ name }) {
+  renderItem(city) {
     return (
       <View style={styles.item}>
-        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{city}</Text>
       </View>
     );
   }
@@ -20,8 +32,8 @@ export default class App extends React.Component {
    return(
      <FlatList style={styles.container}
           renderItem={({item})=> this.renderItem(item)}
-          keyExtractor={(item)=>item.name}
-          data={this.fruits}
+          keyExtractor={item=>item}
+          data={this.state.cities}
      />
    );
  }
