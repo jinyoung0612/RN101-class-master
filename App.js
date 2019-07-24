ef414ad24e24da11dd369a4482b5a1a628b2e183
jinyoung <bmdj123@gmail.com>
2019-07-25 01:58:06 +0900

    20. ScrollView

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, ScrollView } from 'react-native';
 import { Constants }  from 'expo';
 
 export default class App extends React.Component {
  render(){
    return(
-     <View style={styles.container}>
+     <ScrollView style={styles.container}>
       <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}}/>
       <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}}/>
       <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}}/>
@@ -17,7 +17,7 @@ export default class App extends React.Component {
       <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}}/>
       <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}}/>
       <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}}/>
-     </View>
+     </ScrollView>
    );
  }
 }
