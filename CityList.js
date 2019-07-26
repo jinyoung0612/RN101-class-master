852bd23bfc39017699a49cee1bbdb9dea4d1dee0
jinyoung <bmdj123@gmail.com>
2019-07-26 18:33:49 +0900

    34. Navigate from city list

--- CityList.js
+++ CityList.js
@@ -6,7 +6,7 @@ export default class CityList extends React.Component {
   static navigationOptions ={
     title: 'Cities',
   };
-  
+
   constructor(props) {
     super(props);
 
@@ -28,6 +28,7 @@ export default class CityList extends React.Component {
 
   onPressCity(item) {
     console.log('onPressCity =', item);
+    this.props.navigation.navigate('Detail');
   }
 
   renderItem(city) {
